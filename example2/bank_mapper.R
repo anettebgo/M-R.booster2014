63ddd91a0326df1a25e552e196f38d89ea1d65e1
anettebgo <anette.bergo@gmail.com>
2014-03-09 14:54:02 +0100

    fixed bug where bank was only reading every second line

--- example2/bank_mapper.R
+++ example2/bank_mapper.R
@@ -18,7 +18,7 @@ emit <- function(lines){
   input <- file("stdin", open="r")
   
   names <- c("id", "date", "vendor","currency","amount")
-  cols = as.list(vector(length=7, mode="character"))
+  cols = as.list(vector(length=5, mode="character"))
   names(cols) <- names
    #read input in reasonable chunks and apply emit to each chunk
   hsTableReader(file=input, cols, ignoreKey=TRUE, chunkSize=10000, FUN=emit, sep="\t")
--- example2/post_mapper.R
+++ example2/post_mapper.R
@@ -7,8 +7,8 @@ library(getopt)
 emit <- function(lines){
   apply(lines, 1, function(x){
     amount <- as.integer(x[9])
-    country <- as.character(x[6])
-    if(amount > 200 && country != "Norge"){
+    sender.country <- as.character(x[6])
+    if(amount > 200 && sender.country != "Norge"){
       write(paste(x[2], "1", sep="\t"), file=stdout())
     }
   })
@@ -17,7 +17,8 @@ emit <- function(lines){
 (function() {
   input <- file("stdin", open="r")
 
-  names <- c("id", "order.date", "delivery.date", "sender.name","sender.address","sender.country","recipient.name", "recipient.address", "declared.value")
+  names <- c("id", "order.date", "delivery.date", "sender.name","sender.address","sender.country"
+             ,"recipient.name", "recipient.address", "declared.value")
   cols = as.list(vector(length=9, mode="character"))
   names(cols) <- names
    #read input in reasonable chunks and apply emit to each chunk
