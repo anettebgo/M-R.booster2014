4e71a8072daf89ee76a4e1884593df612582c993
Siv Midtun Hollup <siv.hollup@gmail.com>
2014-03-11 21:10:01 +0100

    R_commands.R updated according to R intro

--- extras/R_commands.R
+++ extras/R_commands.R
@@ -17,13 +17,13 @@ sum(multiple)
 sum(single, multiple)
 
 afew = c(2,3,4)
-acouple = c(2,3)
+acouple = c(3,4)
 
 multiple * multiple
 multiple * acouple
 multiple * afew
 
-raw_data  = read.table("~/git/M-R.booster2014/example2/bank_output.csv")
+raw_data  = read.table("~/git/M-R.booster2014/extras/test.csv")
 
 plot(multiple)
 lines(multiple)
@@ -33,6 +33,13 @@ lines(multiple, col="red")
 pdf("test.pdf")
 plot(multiple)
 lines(multiple, col="blue")
+points(acouple, col="green", pch=2)
 dev.off()
 
+a.sum = function(input) {
+    sum.of.input = sum(input)
+    return (sum.of.input)
+}
+
+cats = c("bob", "scardi", "angry")
 sapply(cats, FUN=function(kitty){paste(kitty, 'cat')})
